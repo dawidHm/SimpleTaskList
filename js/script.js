7efebfe8758afc2bde563cfd549b2e94a227aa8d
Dawid Humienik <dawid.humienik@gmail.com>
2022-01-18 22:41:52 +0100

    adding new tasks, checking if input is empty and diffrent style for done tasks added

--- js/script.js
+++ js/script.js
@@ -2,22 +2,50 @@
     const tasksList = [
         {
             content: "sample task 1",
-            done: false,
+            done: true,
         },
     ]; 
 
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasksList) {
-            htmlString += `<li>${task.content}</li>`;
+        for(const singleTask of tasksList) {
+            htmlString += `<li${singleTask.done ? " style=\"text-decoration: line-through\"" : ""}>${singleTask.content}</li>`;
         }
 
         document.querySelector(".js-tasksList").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasksList.push(
+            {
+            content: newTaskContent,
+            done: false,
+            }
+        );
+
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-addTask").value.trim();
+        
+        if (newTaskContent === "") {
+            return;
+        } else {
+            addNewTask(newTaskContent);
+        }
+
+    }
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
